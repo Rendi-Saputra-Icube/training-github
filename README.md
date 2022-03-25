fffaa3d2938747d6a1a14fb924b84ee0bb069949
Rendi-Saputra-Icube <rendi.saputra@mail.sirclo.com>
2022-03-25 05:15:06 +0000

    add new readme and modify

---
+++ README.md
@@ -0,0 +1,3 @@
+# Art Do
+
+## Commiting update readme
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
    <head>
      <title>Art Do</title>
+     <link rel="stylesheet" href="index.css">
    </head>
    <body>
      <h1>Training Github & composer today!</h1>
